6a78a1a4ff292080c876f6546b2fc3637d098bf6
rickeyd1 <rickjay0@gmail.com>
2015-04-26 13:51:32 -0400

    Update Reworkedpegscode.py

--- Reworkedpegscode.py
+++ Reworkedpegscode.py
@@ -5,26 +5,26 @@ win = GraphWin("Pegs", 300,400)
 #First making the random coordinates for the center point of the pegs
 def main():
     num = 7 # Ian feel free to change this value for the number of pegs.
+    pegs = []
     for p in range(num):
         #Row 1
         x = randrange(40,300,85) #Random x point
         y = 100
-        peg1 = Circle(Point(x,y), 12)
-        peg1.setFill("Light Green")
-        peg1.draw(win)
+        pegs.append(Circle(Point(x,y), 12))
         
         #Row 2
         x = randrange(40,300,85) #Random x point
         y = 200
-        peg2 = Circle(Point(x,y), 12)
-        peg2.setFill("Light Green")
-        peg2.draw(win)
+        pegs.append(Circle(Point(x,y), 12))
         
         #Row 3
         x = randrange(40,300,85) #Random x point
         y = 300
-        peg3 = Circle(Point(x,y), 12)
-        peg3.setFill("Light Green")
-        peg3.draw(win)
+        pegs.append(Circle(Point(x,y), 12))
+
+    for i in range(len(pegs)):
+        print(pegs[i])
+        pegs[i].setFill('Light Green')
+        pegs[i].draw(win)
         
 main()
